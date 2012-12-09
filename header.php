1e9cdf1feb57cb907460b0a971bac6c578e5ab3b
croata <croata_08@hotmail.com>
2012-12-09 12:01:20 -0200

    Update header.php
    rename #div page for #div content

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="content" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
